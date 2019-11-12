2c1b45709adf3cdb71b84755c1e664b0b3a66343
veronikaro <veronikarovnik@gmail.com>
2019-11-12 23:19:41 +0200

    Interface for mutation function

--- GeneticAlgorithms.py
+++ GeneticAlgorithms.py
@@ -18,8 +18,23 @@ def generate_population(l, N, X, Y, coding="binary"):
         return None
     return general_population
 
-def calculate_chromosome_fitness(chromosome, fitness_func="hemming distance"):
-    pass
+'''Comparing two binary strings of equal length, Hamming distance is the number of bit positions in which the two bits are different.
+In order to calculate the Hamming distance between two strings, we perform their XOR operation and then count the total number of 1s in the resultant string.
+'''
+def calculate_chromosome_fitness(chromosome, fitness_func="hamming distance"):
+    if fitness_func=="hamming distance":
+        pass
+    else:
+        pass
+
+def mutation(N, l):
+    px = 1/(10*l)
+    pm = px
+    #pm = px + 0.2*px
+    #pm = px - 0.2*px
+    #pm = px/2
+    #pm = px/10
+    #pm = px/100
 
 def tournament_selection(t):
     if t == 2:

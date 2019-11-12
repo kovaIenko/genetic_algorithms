0682d1849370e34fae332333f8bf0005fcb36b6d
veronikaro <veronikarovnik@gmail.com>
2019-11-12 22:57:44 +0200

    Initial draft

--- GeneticAlgorithms.py
+++ GeneticAlgorithms.py
@@ -1,26 +1,35 @@
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
 
+# generate initial population of size N. each chromosome has a length l
 
-# RWS
-def selRoulette(name):
-    print(name)
+# X is a percentage of chromosomes that contain only 0
+# Y is a percentage of chromosomes that are generated using a uniform distribution
 
+def generate_population(l, N, X, Y, coding="binary"):
+    if coding == "binary":
+        x = int(X * N)
+        y = int(Y * N)
+        population_with_zeros = np.random.randint(0, 1, (x, l))
+        uniform_population = np.random.randint(0, 2, (y, l))
+        general_population = np.concatenate((population_with_zeros, uniform_population), axis=0)
+    else:
+        return None
+    return general_population
 
-# generate the population
-def init(len, numbs):
-    for i in range(numbs):
-        print(i)
+def calculate_chromosome_fitness(chromosome, fitness_func="hemming distance"):
+    pass
 
-def healthOf():
-    print()
+def tournament_selection(t):
+    if t == 2:
+        pass
+    elif t == 4:
+        pass
+    elif t == 12:
+        pass
 
-# distance between two chromosome
-def calcDistance(left, right):
-    print(left)
-
-
-selRoulette('Ruslan')
-
-
-
-
+# testing
+pop = generate_population(10, 100, 0.5, 0.5)
+print(pop[:-20])
 

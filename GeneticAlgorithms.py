eb97311bdf1daf27497746e5efe7b89387eec8fb
kovaIenko <ruskov004@gmail.com>
2019-12-04 01:08:26 +0200

    add some key functions for stats

--- GeneticAlgorithms.py
+++ GeneticAlgorithms.py
@@ -4,7 +4,7 @@ import numpy as np  # linear algebra
 from collections import Counter
 from decimal import *
 from operator import attrgetter
-getcontext().prec = 4
+getcontext().prec = 6
 
 
 # generate initial population of size N. each chromosome has a length l
@@ -41,7 +41,7 @@ def selRoulette(list, list_of_health):
     probability = []
     chosen = []
     for ch in list:
-        prob = Decimal(healthOf(ch))/ Decimal(sumH)
+        prob = Decimal(healthOf(ch))/Decimal(sumH)
         probability.append(prob)
     cumsum = np.cumsum(probability)
     cumsum[len(cumsum)-1] = Decimal(1.)
@@ -54,15 +54,6 @@ def selRoulette(list, list_of_health):
                 break
     return chosen, list_of_health
 
-'''
-l = 4
-N = 4
-pop = generate_population(l, N, 0, 1)
-print(selRoulette(pop))
-print("----------------")
-'''
-
-
 # 0 -> 1 or 1 -> 0
 def turnOverGen(ch, indOfGen):
     if ch[indOfGen] == 1:
@@ -73,19 +64,9 @@ def turnOverGen(ch, indOfGen):
 
 
 def mutation(list, percentage, l, list_of_health):
-    #print(percentage)
-    # pm = px + 0.2*px
-    # pm = px - 0.2*px
-    # pm = px/2
-    # pm = px/10
-    # pm = px/100
     count_ = len(list) * l
-    #print(count_)
     numbOfMut = count_ * percentage
-    #print("NumMut: ")
-    #print(numbOfMut)
     indexes = []
-    # generate the indexes which chromosomes will be mutated
     for i in range(int(numbOfMut)):
         u = int(random.random() * count_)
         if u < l - 1:
@@ -113,21 +94,11 @@ def hamming(chaine1, chaine2):
     return sum(c1 != c2 for c1, c2 in zip(chaine1, chaine2))
 
 
-# return the map [ distance : frequency ]
-def calc_all_distances(list, N, l):
-    frequency = { new_list: 0 for new_list in range(l + 1)}
-    for i in range(0, N):
-        for j in range(i + 1, N):
-            dis = hamming(list[i], list[j])
-            frequency[dis] = frequency.get(dis) + 1
-    return frequency
-
-
 # print(calc_all_distances(["01010", "10101"], 2, 5))
 
 
 def healthMean(N, list_health):
-    return sum(list_health)/N
+    return Decimal(sum(list_health)/N)
 
 
 ### execute the mutation
@@ -168,7 +139,87 @@ def tournament_selection(population, t):
   proportion_of_different_genes = scipy.spatial.distance.hamming(sample[0], sample[1])
   print("{}% of different genes".format(proportion_of_different_genes * 100))
   print("The number of different genes is {0}".format(int(proportion_of_different_genes * l)))
-  '''
+ 
+ '''
+
+
+CONST_STOP_ALGORITHM = 2000
+CONST_STOP_ALGORITHM_BY_MEAN_HEALTH = 0.0001
+CONST_NUMB_GETTING_INFO = 10
+
+
+def should_be_stopped(worksheet, pop, N, l, i, sum_mean_health, current_mean_health):
+    if i % CONST_NUMB_GETTING_INFO == 0 and i != 0:  # each 11n iterations
+        save_to_file(worksheet, calc_all_distances(pop, N, l), i)
+        last_10_mean_health = sum_mean_health / CONST_NUMB_GETTING_INFO
+        if Decimal(current_mean_health) - Decimal(last_10_mean_health) < CONST_STOP_ALGORITHM_BY_MEAN_HEALTH:
+            print("the_best_individual_distances")  # wild type
+            the_best_individual_distances = distances_for_wild_type(pop)
+            print(the_best_individual_distances)
+            print("Algorithm was stopped: there is mistake ")
+            return True
+    if i+1 == CONST_STOP_ALGORITHM:
+        print("the_best_individual_distances")  # wild type
+        the_best_individual_distances = distances_for_wild_type(pop)
+        print(the_best_individual_distances)
+    return False
+
+
+def init_health_list(list):
+    list_of_health = []
+    for ch in list:
+        list_of_health.append(healthOf(ch))
+    return list_of_health
+
+# return the map [ distance : frequency ]
+def calc_all_distances(list, N, l):
+    frequency = { new_list: 0 for new_list in range(l + 1)}
+    for i in range(0, N):
+        for j in range(i + 1, N):
+            dis = hamming(list[i], list[j])
+            frequency[dis] = frequency.get(dis) + 1
+    return frequency
+
+
+def calc_hamming_to_ideal(list_health, l):
+    frequency = {new_list: 0 for new_list in range(l + 1)}
+    for ch_health in list_health:
+        dis = l-ch_health
+        frequency[dis] = frequency.get(dis) + 1
+        return frequency
+
+
+def distances_for_wild_type(last_pop):
+     frequencies = {new_list: 0 for new_list in range(l + 1)}
+     frequency = [0]*l
+     for ind in last_pop:
+        for i, gen in enumerate(ind):
+          if gen == 0:
+            frequency[i] += 1
+          else:
+            frequency[i] -= 1
+     the_best= []
+     for f in frequency:
+         if f >= 0:
+             the_best.append(0)
+         else:
+             the_best.append(1)
+     for ch in last_pop:
+         dis = hamming(ch, the_best)
+         frequencies[dis] = frequencies.get(dis) + 1
+     return frequencies
+
+
+def bestHealth(list_of_health):
+    return Decimal(max(list_of_health))
+
+def deviation_meanHealth_and_optimum(list_of_health, N, l ):
+    mean = healthMean(N, list_of_health)
+    return Decimal(l) - mean
+
+def deviation_bestHealth_and_optimum(list_of_health, l):
+    return bestHealth(list_of_health) - l
+
 
 import xlsxwriter
 
@@ -186,38 +237,26 @@ def save_to_file(worksheet, dict, iterate):
         worksheet.write(row, col, dict.get(key))
         col = col + 1
 
+def save_to_file_each_iter(worksheet, iteration, list_of_health, N, l):
+    meanHealth = healthMean(N, list_of_health)
+    Healthbest = bestHealth(list_of_health)
+    val_dev_meanHealth = deviation_meanHealth_and_optimum(list_of_health, N, l)
+    val_dev_bestHealth = deviation_bestHealth_and_optimum(list_of_health, l)
+    iteration
 
-CONST_STOP_ALGORITHM = 200000
-CONST_STOP_ALGORITHM_BY_MEAN_HEALTH = 0.0001
-CONST_NUMB_GETTING_INFO = 10
 
 
-def should_be_stopped(worksheet, pop, N, l, i, sum_mean_health, current_mean_health):
-    if i % CONST_NUMB_GETTING_INFO == 0 and i != 0:  # each 11n iterations
-        # save_to_file(worksheet, calc_all_distances(pop, N, l), i)
-        last_10_mean_health = sum_mean_health / CONST_NUMB_GETTING_INFO
-        if Decimal(current_mean_health) - Decimal(last_10_mean_health) < CONST_STOP_ALGORITHM_BY_MEAN_HEALTH:
-            print("Algorithm was stopped: there is mistake ")
-            return True
-    return False
-
-def init_health_list(list):
-    list_of_health = []
-    for ch in list:
-        list_of_health.append(healthOf(ch))
-    return list_of_health
 
 def execution(l, N):
     workbook = xlsxwriter.Workbook('data.xls')
     worksheet = workbook.add_worksheet()
     pop = generate_population(l, N, 0, 1)
+    print(distances_for_wild_type(pop))
     pm = 1 / (10 * l)
-
     sum_mean_health = Decimal(0)
     list_health = init_health_list(pop)
-
     for i in range(CONST_STOP_ALGORITHM):
-        mean = Decimal(healthMean(N, list_health))
+        mean = healthMean(N, list_health)
         if should_be_stopped(worksheet, pop, N, l, i, sum_mean_health, mean):
             sum_mean_health = 0
             break
@@ -228,11 +267,39 @@ def execution(l, N):
         pop, list_health = mutation(pop, pm, l, list_health)
         print("after mutation")
         print(pop)
-        print("- - -  - - - - - - - -- - - - - - - - - - - - - - - - - - - - - ")
+        print("- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
     workbook.close()
 
 
 l = 10
-N = 200000
+N = 300
 execution(l, N)
 
+
+def execution2(l, N, X ,Y):
+    workbook = xlsxwriter.Workbook('data.xls')
+    worksheet = workbook.add_worksheet()
+    pop = generate_population(l, N, X, Y)
+    pm = 1 / (10 * l)
+    sum_mean_health = Decimal(0)
+    list_health = init_health_list(pop)
+    for i in range(CONST_STOP_ALGORITHM):
+        mean = healthMean(N, list_health)
+        if should_be_stopped(worksheet, pop, N, l, i, sum_mean_health, mean):
+            sum_mean_health = 0
+            break
+        sum_mean_health += mean
+        pop, list_health = selRoulette(pop, list_health)
+        print("after roulette")
+        print(pop)
+        pop, list_health = mutation(pop, pm, l, list_health)
+        print("after mutation")
+        print(pop)
+        print("- - -  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ")
+    workbook.close()
+
+l = 10
+N = 2000
+X = 0.9
+Y = 0.1
+#execution2(l, N, X, Y)

f67ae8241cfb1c7cf932f4f4300484cbaddc93b7
veronikaro <veronikarovnik@gmail.com>
2019-12-22 16:04:43 +0200

    alternative saving to csv

--- GeneticAlgorithms.py
+++ GeneticAlgorithms.py
@@ -417,6 +417,7 @@ def build_line_graph(health_values, N, l, x, y, selection_type, pm, attempt, ini
 
  '''
 
+# Nika: I suppose we don't need this method anymore
 
 def init_health_list(pop, l, N, method=1):
     list_of_health = []
@@ -516,6 +517,7 @@ def save_to_file(worksheet, dict, iterate):
 
 def save_to_file_the_end(attempt, iteration, list_of_health, N, l, pm, type_of_selection, X, Y, init_type,
                          params_tour=-1, arr_neutral_iter=None):
+    '''
     row = []
     row_map = {"attempt": attempt, "init_type": init_type, "N": N, "l": l, "X": X * 100, "Y": Y * 100,
                "iteration": iteration, "pm": pm}
@@ -551,9 +553,10 @@ def save_to_file_the_end(attempt, iteration, list_of_health, N, l, pm, type_of_s
         writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(row)
     file.close()
+    '''
 
     # Another variant with dynamically defining csv columns
-    '''
+
     csv_columns = ['attempt', 'init_type', 'iteration', 'N', 'l', 'X', 'Y', 'pm', 'type_of_selection', 'health_mean', 'health_best', 'deviation_meanHealth_and_optimum', 'deviation_bestHealth_and_optimum', 'percent_polym_genes', 'arr_neutral_iter']
     row_map = {"attempt": attempt, "init_type": init_type, "N": N, "l": l, "X": X*100, "Y": Y*100, "iteration": iteration, "pm": pm}
     if type_of_selection == "Tournament":
@@ -577,7 +580,7 @@ def save_to_file_the_end(attempt, iteration, list_of_health, N, l, pm, type_of_s
         print("I/O error")
 
     print(row_map)
-    '''
+
 
 
 def should_be_stopped(i, sum_mean_health, current_mean_health):
